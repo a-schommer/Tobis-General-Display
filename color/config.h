3a79a5e1cf11b0cb95002c1f06afdfc6993966be
Arnold Schommer <50968248+a-schommer@users.noreply.github.com>
2019-12-25 23:20:51 +0100

    create version vor color displays

--- color/config.h
+++ color/config.h
@@ -1,1 +1,34 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
 
+#define PROJECT_TITLE   "Tobi's General Display"
+// for "debug" output on serial: baudrate to set up
+#define BAUDS 115200
+
+// this define defines a complete declaration&definition statement (excluding the final ;) 
+// to construct some u8g2/ucg object specific to the hardware being used:
+// the object name *must* be u8g2/ucg (as lots of source relies on this)
+// GuoYun + SSD1351:
+#define UCG_CONSTRUCTION Ucglib_SSD1351_18x128x128_HWSPI ucg(/*cd=*/ 17, /*cs=*/ 21, /*reset=*/ 16)
+
+// name & password of the wifi to create if we can't join any:
+#define FALLBACK_APSTANAME  "ESP_Config"
+#define FALLBACK_WIFIPWD    "EspWiFiDisplay"
+// hostname (for mdns)
+#define FALLBACK_HOSTNAME   "ESP32"
+
+// max. length of wifi passwords (to save)
+#define WIFIPWDLEN  25
+// max. length of wifi nets (to save)
+#define APSTANAMELEN 20
+
+// how long (ms) should each frame be showed during the slideshow?
+#define SLIDESHOW_PERIOD 3000
+
+// (max.) buffer slots for images:
+#define SLIDESHOW_MAX_IMAGES    64
+// (max.) filename length (i did not find a define for how long an SPIFFS filename may be); longer filenames will be cut to this!
+#define MAX_FILENAME_LEN        32
+
+
+#endif _CONFIG_H
---
+++ color/gfxlayer.h
@@ -0,0 +1,34 @@
+/*
+
+Tobis General Display
+
+gfxlayer.h
+
+graphics relevant "interfaces" - mostly defined as inline functions or macros.
+
+ucg variant
+
+*/
+
+#ifndef GFXLAYER_H
+#define GFXLAYER_H
+
+inline void gfx_flushBuffer(void)                               { }     // write the whole buffer to the display - only needed if the gfx system uses a framebuffer instead of writing everything to the display immediately
+inline void gfx_clearScreen(void)                               { ucg.clearScreen(); }
+inline uint16_t gfx_getScreenWidth(void)                        { return ucg.getWidth(); }
+inline uint16_t gfx_getScreenHeight(void)                       { return ucg.getHeight(); }
+inline void gfx_setPixel(uint16_t x, uint16_t y)                { ucg.drawPixel(x, y); }
+inline void gfx_setPixelColor(uint8_t r, uint8_t g, uint8_t b)  { ucg.setColor(r, g, b); }
+inline void gfx_setTextColor(uint8_t r, uint8_t g, uint8_t b)   { ucg.setColor(r, g, b); }
+inline void gfx_drawString(uint16_t x, uint16_t y, const char *str)   { ucg.drawString(x,y, 0, str); }
+
+inline void gfx_init(void)  // calls gfx_clearScreen() and gfx_flushBuffer(); therefore defined afterwards
+{
+    ucg.begin(UCG_FONT_MODE_TRANSPARENT);
+    ucg.setFont(ucg_font_ncenR10_tr);
+    ucg.setFontMode(UCG_FONT_MODE_TRANSPARENT);
+    gfx_clearScreen();
+    gfx_flushBuffer();
+}
+
+#endif GFXLAYER_H
